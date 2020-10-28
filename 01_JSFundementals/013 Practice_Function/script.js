90a89dd630fac343274ebcb204472fd35f32df95
YaroslavShevchuk <crosskyiv@gmail.com>
2020-10-28 16:03:13 +0200

    'nnm'

--- 01_JSFundementals/013 Practice_Function/script.js
+++ 01_JSFundementals/013 Practice_Function/script.js
@@ -6,9 +6,7 @@
 false - выводит в консоль главный объект программы
 
 3) Создать функцию writeYourGenres в которой пользователь будет 3 раза отвечать на вопрос
-"Ваш любимый жанр под номером ${номер по порядку}". Каждый ответ записывается в массив данных
-genres
-
+"Ваш любимый жанр под номер
 P.S. Функции вызывать не обязательно*/
 
 'use strict';
@@ -28,7 +26,7 @@ function start(){
 
 }
 
-start()
+// start()
 
 
 
@@ -41,18 +39,55 @@ start()
     };
 
 
+    function rememberMyFilms(){
+        for (let i=0; i<2; i++){
+            const a = prompt('The last you seen?'),
+                b= prompt('Your num?');
 
-    if (personalMovieDB.count < 10){
-        console.log('You are weak viewer ')
-    }else if ( personalMovieDB.count >= 10 && personalMovieDB.count < 30 ){
-        console.log('You are common viewer')
-    }else if ( personalMovieDB.count >= 30 ){
-        console.log('You are movie buff')
-    }else{
-        console.log('Just Error!') }
+            if (a != null && b != null && a != '' && b !='' && a.length < 50 ){
+                personalMovieDB.movies[a] = b;
+                console.log('done')
+            }else{
+                console.log('error')
+                i--;
+            }
+
+        }
+    }
+
+    // rememberMyFilms();
 
 
-console.log(personalMovieDB)
+    function detectPersonal(){
+        if (personalMovieDB.count < 10){
+            console.log('You are weak viewer ')
+        }else if ( personalMovieDB.count >= 10 && personalMovieDB.count < 30 ){
+            console.log('You are common viewer')
+        }else if ( personalMovieDB.count >= 30 ){
+            console.log('You are movie buff')
+        }else{
+            console.log('Just Error!') }
+}
+
+    // detectPersonal()
+
+    function showMyDb(hidden){
+          if (!hidden) {
+              console.log(personalMovieDB)
+          }
+    }
+
+    showMyDb(personalMovieDB.privat)
+
+
+    function writeYourGenres(){
+        for (let i=1; i<=3; i++){
+            personalMovieDB.genres[ i ] = prompt(`Your favorite genre? ${i}`);
+
+        }
+    }
+
+    writeYourGenres()
 
 
 
