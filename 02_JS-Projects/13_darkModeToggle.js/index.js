e43f2534f675f957364d48585aeafc12535b368e
yaroprof <crosskyiv@gmail.com>
2020-11-01 18:13:28 +0200

    'dakrMOdeChange'

---
+++ 02_JS-Projects/13_darkModeToggle.js/index.js
@@ -0,0 +1,34 @@
+
+// const toggle = document.getElementById('toggle');
+// toggle.addEventListener('change', (e)=>{
+//     document.body.classList.toggle('dark', e.target.checked);
+// });
+
+
+const toggle = document.getElementById('toggle');
+
+toggle.addEventListener('change', (e)=>{
+    document.body.classList.toggle('dark', e.target.checked);
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
