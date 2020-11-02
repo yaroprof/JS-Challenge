827640324786ee20f4771cdca82fd1805356081a
yaroprof <crosskyiv@gmail.com>
2020-11-02 14:17:41 +0200

    'taskQuestion-1'

---
+++ 03_JS-Tasks/01.1_TaskQuestions.js
@@ -0,0 +1,99 @@
+
+// 01
+/*
+let x = 5;
+console.log(x++);   // 5  - post increment; first - show result then - increase
+
+ */
+
+
+// 02
+// console.log([] + false - null)   // NaN
+/*
+// 1
+console.log(typeof ([] + false))  // string  [] - string + false = string // dynamic typing
+// 2
+console.log(typeof ('' - null))  // NaN
+ */
+
+/*
+// 03
+let y = 1; let x = y =2;   // послідовне привласнення з права наліво у примітивах, де
+console.log(x) // 2
+
+ */
+
+/*
+// 04
+console.log([] + 1 + 2);  // 12   // [] here - is a string in this expression; then - dynamic typing for : 1 and 2; then 1 and -> string
+ */
+
+/*
+// 05
+console.log("1"[0])  // 1
+ */
+
+/*
+// 06
+console.log(2 && 1 && null && 0 && undefined);   // null // && - operator all time search first-step: false element - element from left to right
+ */
+
+/*
+// 07
+let a, b;
+console.log( !!( a && b ) === (a && b)); // false  // !! - converts expression on boolean element => this 2parts not equal
+*/
+
+/*
+// 08
+console.log(( null || 2 && 3 || 4 ));  // 3   //  || - operator all time search first-step: true element; in this case && operator is more high priority
+
+ */
+
+/*
+//09
+console.log((a = [1, 2, 3]) === (b = [1, 2, 3]))  // false; a and b not the same variables
+ */
+
+/*
+// 10
+console.log(+'Infinity');   // Infinity // type conversion: unary plus convers. '' on a number
+ */
+
+/*
+// 11
+console.log('boy' > 'apple');   // true
+
+ */
+
+/*
+//12
+console.log(0 || "" || 2 || undefined || true || falsе); // 2
+ */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
