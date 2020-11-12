42d7907a40b10a20d35bfbe597538414b766b53c
yaroprof <crosskyiv@gmail.com>
2020-11-12 14:08:26 +0200

    'dateMethods'

--- 01_JSFundementals/0.1_JS-practice/01.3 siteFood/js/index.js
+++ 01_JSFundementals/0.1_JS-practice/01.3 siteFood/js/index.js
@@ -1,43 +1,39 @@
 window.addEventListener('DOMContentLoaded', () =>{
 
-    const tabs = document.querySelectorAll('.tabheader__item');
-    const tabsContent = document.querySelectorAll('.tabcontent');
-    const tabsParent = document.querySelector('.tabheader__items');
+    const tabs = document.querySelectorAll('.tabheader__item'),
+        tabsParent = document.querySelector('.tabheader__items'),
 
+        tabsContent = document.querySelectorAll('.tabcontent');
 
-
-    function hideTabContent(){
-        tabsContent.forEach(item =>{
-            item.classList.add('hide');
-            item.classList.remove('show', 'fade');
-        });
-        tabs.forEach(item =>{
-            item.classList.remove('tabheader__item_active');
-        });
-    }
-
+     function hideTabContent(item){
+         tabsContent.forEach(item =>{
+             item.classList.add('hide', 'fade');
+             item.classList.remove('show', 'fade');
+         });
+         tabs.forEach(item =>{
+             item.classList.remove('tabheader__item_active');
+         });
+     }
     function showTabContent(i = 0){
-        tabsContent[i].classList.add('show', 'fade' );
-        tabsContent[i].classList.remove('hide');
-        tabs[i].classList.add('tabheader__item_active');
+         tabsContent[i].classList.add('show');
+         tabsContent[i].classList.remove('hide');
+         tabs[i].classList.add('tabheader__item_active');
     }
 
     hideTabContent();
-    showTabContent();
-
-
+     showTabContent();
 
     tabsParent.addEventListener('click', (event) => {
         const target = event.target;
-
-        if (target && target.classList.contains('tabheader__item')){
-            tabs.forEach((item, i) => {
-                if (target == item){
+        if (target && target.classList.contains('tabheader__item')) {
+            tabs.forEach((item, i) =>{
+                if ( target == item ){
                     hideTabContent();
                     showTabContent(i);
                 }
-            });
+            })
         }
-
     });
-} );
+
+
+});
---
+++ 01_JSFundementals/0.1_JS-practice/01.5 datePractice/index.js
@@ -0,0 +1,36 @@
+// const now = new Date();
+
+// GTEer - methods
+
+// console.log(now.getFullYear())
+// console.log(now.getDay());
+// console.log(now.getHours());
+// console.log(now.getMonth());
+// console.log(now.getTime());
+// console.log(now.getTimezoneOffset());
+// console.log(now);
+
+// SETer - methods
+/*
+const now = new Date();
+console.log(now.setHours(40));  // 2020-11-12T16:35:07.984Z
+console.log(now)
+
+ */
+
+// Date - example
+
+let  start= new Date();
+
+for(let i= 0; i<100000; i++){
+    let some = i ** 1;
+}
+
+let end= new Date();
+
+alert (`The loop working for ${ end - start } milisecond`);
+
+
+
+
+
