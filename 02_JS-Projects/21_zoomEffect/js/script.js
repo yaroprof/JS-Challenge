f085f71c49225e761e46464f7d0745ba00098a49
yaroprof <crosskyiv@gmail.com>
2021-01-16 15:48:53 +0200

    'soundPanel'

--- 02_JS-Projects/21_zoomEffect/js/script.js
+++ 02_JS-Projects/21_zoomEffect/js/script.js
@@ -1,14 +1,58 @@
+/*
 const container = document.getElementById('container');
 const img = document.querySelector('img');
 
-container.addEventListener('mousemove', (e) =>{
+container.addEventListener('mousemove', function (e){
     const x = e.clientX - e.target.offsetLeft;
     const y = e.clientY - e.target.offsetTop;
 
-    console.log(x,y);
+    img.style.transformOrigin = `${x}px ${y}px`;
+    img.style.transform = 'scale(2)'
+
+})
+
+ */
+
+const container = document.getElementById('container');
+const img = document.querySelector('img');
+
+container.addEventListener('mousemove', function (e){
+    const x = e.clientX - e.target.offsetLeft;
+    const y = e.offsetY - e.target.offsetTop;
 
     img.style.transformOrigin = `${x}px ${y}px`;
-    img.style.transform = "scale(2)";
-});
+    img.style.transform = 'scale(2)';
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
---
+++ 02_JS-Projects/22_soundPanel/script.js
@@ -0,0 +1,64 @@
+const sounds = ['applause','boo','gasp', 'tada', 'victory','wrong'];
+
+sounds.forEach((sound) =>{
+    const btn = document.createElement('button');
+    btn.classList.add('btn');
+    btn.innerText = sound;
+
+    btn.addEventListener('click', ()=>{
+        document.getElementById(sound).play();
+    })
+
+    document.body.appendChild(btn)
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
